dae53d8f69f1166ae78c3b377d13bfad989ab366
Noa <76524924+Noa-Nussbaum@users.noreply.github.com>
2023-04-17 13:19:15 +0000

    Finished tests

---
+++ Test.cpp
@@ -0,0 +1,279 @@
+#include "doctest.h"
+#include "sources/Fraction.hpp"
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
+#include <stdexcept>
+
+using namespace std;
+
+namespace ariel{
+
+TEST_CASE("Initiation"){
+
+    Fraction one(5,3);
+    CHECK(one==5/3);
+
+    Fraction two(8,2);
+    CHECK(two==8/2);
+
+    Fraction three(14,21);
+    CHECK(three==14/21);
+
+    // CHECK_THROWS(Fraction four(14,0));
+
+}
+TEST_CASE("Addition"){
+
+    Fraction one(5,3);
+    Fraction two(8,2);
+    Fraction three(17,3);
+
+    CHECK(one+two==three);
+    CHECK(one+one==2*one);
+    CHECK(two+two==2*one);
+    
+}
+
+TEST_CASE("Subtraction"){
+
+    Fraction one(5,3);
+    Fraction two(8,2);
+    Fraction three(-7,3);
+
+    CHECK(one-two==three);
+    CHECK(two-two==0);
+    CHECK(three-three==0);
+
+}
+
+TEST_CASE("Multiplication"){
+
+    Fraction one(5,3);
+    Fraction two(8,2);
+    Fraction three(25,9);
+    Fraction four(20,3);
+    Fraction five(16,1);
+
+    CHECK(one*one==three);
+    CHECK(two*one==four);
+    CHECK(two*two==five);
+
+}
+
+TEST_CASE("Division"){
+
+    Fraction one(5,3);
+    Fraction two(8,2);
+    Fraction three(5,12);
+    Fraction four(7,9);
+    Fraction five(15,28);
+    Fraction six(1,1);
+
+
+    CHECK(one/two==three);
+    CHECK(three/four==five);
+    CHECK(four/four==six);
+    CHECK(five/five==six);
+
+
+}
+TEST_CASE("Equals == "){
+        
+    Fraction one(64,17);
+    Fraction two(64,2);
+    Fraction three(64,17);
+    
+    CHECK(one==three);
+    CHECK_FALSE(one==two);
+
+}
+TEST_CASE("All comparisons"){
+        
+    Fraction one(64,17);
+    Fraction two(64,2);
+    Fraction three(64,17);
+
+    CHECK(one<=three);
+    CHECK(one<=two);
+    CHECK(one<two);
+
+    CHECK_FALSE(one==two);
+    CHECK_FALSE(one>=two);
+    CHECK_FALSE(one>two);
+    
+
+}
+TEST_CASE("++ pre fix"){
+
+    Fraction one(3,1);
+    Fraction two(4,1);
+
+    CHECK(++one == two);
+    CHECK_FALSE(++one == one); 
+
+}
+TEST_CASE("++ post fix"){
+        
+    Fraction one(3,1);
+    Fraction two(4,1);
+
+    CHECK(one++ == one);
+    CHECK_FALSE(one++ == two); 
+
+}
+TEST_CASE("-- pre fix"){
+
+    Fraction one(3,1);
+    Fraction two(4,1);
+
+    CHECK(--one == two);
+    CHECK_FALSE(--one == one); 
+
+}
+TEST_CASE("-- post fix"){
+        
+    Fraction one(3,1);
+    Fraction two(4,1);
+
+    CHECK(one-- == one);
+    CHECK_FALSE(one-- == two); 
+
+}
+
+TEST_CASE("<< operator"){
+        
+        
+
+}
+TEST_CASE(">> operator"){
+        
+        
+
+}
+
+
+////////////////////
+
+// Float and function
+
+////////////////////
+
+
+TEST_CASE("Addition"){
+
+    float one=1.666;
+    Fraction two(8,2);
+    Fraction three(17,3);
+
+    CHECK(one+two==three);
+    CHECK(two+one==three);
+    
+}
+
+TEST_CASE("Subtraction"){
+
+    float one=1.666;
+    Fraction two(8,2);
+    Fraction three(-7,3);
+
+    CHECK(one-two==three);
+
+}
+
+TEST_CASE("Multiplication"){
+
+    float one=1.666;
+    Fraction two(8,2);
+    Fraction four(20,3);
+
+    CHECK(two*one==four);
+    CHECK(one*two==four);
+
+}
+
+TEST_CASE("Division"){
+
+    float one=1.666;
+    Fraction two(8,2);
+    Fraction three(5,12);
+
+    CHECK(one/two==three);
+
+}
+TEST_CASE("Equals == "){
+        
+    float one = 3.764;
+    Fraction two(64,2);
+    Fraction three(64,17);
+    
+    CHECK(one==three);
+    CHECK(three==one);
+    CHECK_FALSE(one==two);
+
+}
+TEST_CASE("All comparisons"){
+        
+    float one = 3.764;
+    Fraction two(64,2);
+    Fraction three(64,17);
+
+    CHECK(one<=three);
+    CHECK(one<=two);
+    CHECK(one<two);
+
+    CHECK_FALSE(one==two);
+    CHECK_FALSE(one>=two);
+    CHECK_FALSE(one>two);
+
+}
+TEST_CASE("++ pre fix"){
+
+    float one = 3;
+    Fraction two(4,1);
+
+    CHECK(++one == two);
+    CHECK_FALSE(++one == one); 
+
+}
+TEST_CASE("++ post fix"){
+        
+    float one = 3;
+    Fraction two(4,1);
+
+    CHECK(one++ == one);
+    CHECK_FALSE(one++ == two); 
+
+}
+TEST_CASE("-- pre fix"){
+
+    float one = 3;
+    Fraction two(4,1);
+
+    CHECK(--one == two);
+    CHECK_FALSE(--one == one); 
+
+}
+TEST_CASE("-- post fix"){
+        
+    float one = 3;
+    Fraction two(4,1);
+
+    CHECK(one-- == one);
+    CHECK_FALSE(one-- == two); 
+
+}
+
+TEST_CASE("<< operator"){
+        
+        
+
+}
+TEST_CASE(">> operator"){
+        
+        
+
+}
+
+
+
+}
--- sources/Fraction.cpp
+++ sources/Fraction.cpp
@@ -6,20 +6,89 @@
 
 namespace ariel{
     Fraction::Fraction(int one,int two){}
-
-    Fraction operator+(Fraction other){
-        Fraction(1,3);
-    }
-
+    Fraction::Fraction(float one){}
     std::ostream& operator<<(std::ostream& os, const Fraction& obj)
     {
         // write obj to stream
         return os;
     }
-    // Fraction operator+(float other, Fraction f){
-    // }
 
     
+    
+    
+    // fraction to fraction
+        Fraction Fraction::operator+(Fraction other){
+            return Fraction(1,3);
+        }
+        Fraction Fraction::operator-(Fraction other){
+            return Fraction(1,3);
+        }
+        Fraction Fraction::operator*(Fraction other){
+            return Fraction(1,3);
+        }
+        Fraction Fraction::operator/(Fraction other){
+            return Fraction(1,3);
+        }
+        bool Fraction::operator==(Fraction other){
+            return true;
+        }
+        bool Fraction::operator>(Fraction other){
+            return true;
+        }
+        bool Fraction::operator<(Fraction other){
+            return true;
+        }
+        bool Fraction::operator>=(Fraction other){
+            return true;
+        }
+        bool Fraction::operator<=(Fraction other){
+            return true;
+        }
+        Fraction Fraction::operator++(){
+            return Fraction(1,3); 
+        }
+        Fraction Fraction::operator--(){
+            return Fraction(1,3);
+        }
+        Fraction Fraction::operator++(int){
+            return Fraction(1,3);
+        }
+        Fraction Fraction::operator--(int){
+            return Fraction(1,3);
+        }
+
+
+
+    // float to fraction
+        Fraction operator+(float other, Fraction f){
+            return Fraction(1,3);
+        }
+        Fraction operator-(float other, Fraction fraction){
+            return Fraction(1,3);
+        }
+        Fraction operator*(float other, Fraction fraction){
+            return Fraction(1,3);
+        }
+        Fraction operator/(float other, Fraction fraction){
+            return Fraction(1,3);
+        }
+        bool operator==(float other, Fraction fraction){
+            return true;
+        }
+        bool operator>(float other, Fraction fraction){
+            return true;
+        }
+        bool operator<(float other, Fraction fraction){
+            return true;
+        }
+        bool operator>=(float other, Fraction fraction){
+            return true;
+        }
+        bool operator<=(float other, Fraction fraction){
+            return true;
+        }
+
+
 
 }
     
